b0234484e3f3d3c31af48be8a7e59fc45ac02874
Ivanio1 <sobolev1van@yandex.ru>
2024-02-19 12:01:07 +0300

    Initial commit

---
+++ src/main/java/ru/ivanio1/App.java
@@ -0,0 +1,13 @@
+package ru.ivanio1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
