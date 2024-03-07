bb90fb7fa00186d2236a3c706062e3f5d5fa2f59
Ivanio1 <sobolev1van@yandex.ru>
2024-03-07 11:24:43 +0300

    fix: add file clearing before tests

--- tpo2/src/test/java/se/ifmo/log/LogFuncTest.java
+++ tpo2/src/test/java/se/ifmo/log/LogFuncTest.java
@@ -1,12 +1,17 @@
 package se.ifmo.log;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import se.ifmo.utils.CsvLogger;
 
+import java.io.File;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class LogFuncTest {
 
     private final Ln ln = new Ln();
@@ -19,13 +24,25 @@ public class LogFuncTest {
     private final double accuracy = 0.01;
     private final double eps = 0.0001;
 
+    @BeforeAll
+    public void clearFiles() {
+        String directoryPath = "src/test/resources/results/log/";
+        File directory = new File(directoryPath);
+        File[] files = directory.listFiles();
+        assert files != null;
+        for (File file : files) {
+            csvLogger.setFilePath(String.valueOf(file));
+            csvLogger.clearFile();
+        }
+    }
+
 
     @ParameterizedTest
     @CsvFileSource(resources = "/InputLog/lnData.csv")
     @DisplayName("ln(x) test")
     void lnTest(Double x, Double trueResult) {
         try {
-            csvLogger.setFilePath("src/test/resources/results/ln.csv");
+            csvLogger.setFilePath("src/test/resources/results/log/ln.csv");
             double result = ln.calculate(x, eps);
             csvLogger.logger(x, result);
             assertEquals(trueResult, result, accuracy);
@@ -39,7 +56,7 @@ public class LogFuncTest {
     @DisplayName("log3(x) test")
     void log3Test(Double x, Double trueResult) {
         try {
-            csvLogger.setFilePath("src/test/resources/results/log3.csv");
+            csvLogger.setFilePath("src/test/resources/results/log/log3.csv");
             double result = log3.calculate(x, eps);
             csvLogger.logger(x, result);
             assertEquals(trueResult, result, accuracy);
@@ -53,7 +70,7 @@ public class LogFuncTest {
     @DisplayName("log2(x) test")
     void log2Test(Double x, Double trueResult) {
         try {
-            csvLogger.setFilePath("src/test/resources/results/log2.csv");
+            csvLogger.setFilePath("src/test/resources/results/log/log2.csv");
             double result = log2.calculate(x, eps);
             csvLogger.logger(x, result);
             assertEquals(trueResult, result, accuracy);
@@ -67,7 +84,7 @@ public class LogFuncTest {
     @DisplayName("log5(x) test")
     void log5Test(Double x, Double trueResult) {
         try {
-            csvLogger.setFilePath("src/test/resources/results/log5.csv");
+            csvLogger.setFilePath("src/test/resources/results/log/log5.csv");
             double result = log5.calculate(x, eps);
             csvLogger.logger(x, result);
             assertEquals(trueResult, result, accuracy);
@@ -81,7 +98,7 @@ public class LogFuncTest {
     @DisplayName("log10(x) test")
     void log10Test(Double x, Double trueResult) {
         try {
-            csvLogger.setFilePath("src/test/resources/results/log10.csv");
+            csvLogger.setFilePath("src/test/resources/results/log/log10.csv");
             double result = log10.calculate(x, eps);
             csvLogger.logger(x, result);
             assertEquals(trueResult, result, accuracy);
@@ -95,7 +112,7 @@ public class LogFuncTest {
     @DisplayName("logarithmic function test")
     void logFuncTest(Double x, Double trueResult) {
         try {
-            csvLogger.setFilePath("src/test/resources/results/logFunc.csv");
+            csvLogger.setFilePath("src/test/resources/results/log/logFunc.csv");
             double result = logarithmicCalculator.calculate(x, eps);
             csvLogger.logger(x, result);
             assertEquals(trueResult, result, accuracy);
--- tpo2/src/test/java/se/ifmo/trig/TrigFuncTest.java
+++ tpo2/src/test/java/se/ifmo/trig/TrigFuncTest.java
@@ -1,12 +1,17 @@
 package se.ifmo.trig;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import se.ifmo.utils.CsvLogger;
 
+import java.io.File;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TrigFuncTest {
 
     private final Sin sin = new Sin();
@@ -20,12 +25,25 @@ public class TrigFuncTest {
     private final double accuracy = 0.1;
     private final double eps = 0.0000001;
 
+    @BeforeAll
+    public void clearFiles() {
+        String directoryPath = "src/test/resources/results/trig/";
+        File directory = new File(directoryPath);
+        File[] files = directory.listFiles();
+        assert files != null;
+        for (File file : files) {
+            csvLogger.setFilePath(String.valueOf(file));
+            csvLogger.clearFile();
+        }
+    }
+
+
     @ParameterizedTest
     @CsvFileSource(resources = "/inputTrig/sinData.csv")
     @DisplayName("sin(x) test")
     void sinTest(Double divisible, Double divider, Double trueResult) {
         try {
-            csvLogger.setFilePath("src/test/resources/results/sin.csv");
+            csvLogger.setFilePath("src/test/resources/results/trig/sin.csv");
             double x = divisible * Math.PI / divider;
             double result = sin.calculate(x, eps);
             csvLogger.logger(x, result);
@@ -40,7 +58,7 @@ public class TrigFuncTest {
     @DisplayName("cos(x) test")
     void cosTest(Double divisible, Double divider, Double trueResult) {
         try {
-            csvLogger.setFilePath("src/test/resources/results/cos.csv");
+            csvLogger.setFilePath("src/test/resources/results/trig/cos.csv");
             double x = divisible * Math.PI / divider;
             double result = cos.calculate(x, eps);
             csvLogger.logger(x, result);
@@ -55,7 +73,7 @@ public class TrigFuncTest {
     @DisplayName("tan(x) test")
     void tanTest(Double divisible, Double divider, Double trueResult) {
         try {
-            csvLogger.setFilePath("src/test/resources/results/tan.csv");
+            csvLogger.setFilePath("src/test/resources/results/trig/tan.csv");
             double x = divisible * Math.PI / divider;
             double result = tan.calculate(x, eps);
             csvLogger.logger(x, result);
@@ -70,7 +88,7 @@ public class TrigFuncTest {
     @DisplayName("cot(x) test")
     void cotTest(Double divisible, Double divider, Double trueResult) {
         try {
-            csvLogger.setFilePath("src/test/resources/results/cot.csv");
+            csvLogger.setFilePath("src/test/resources/results/trig/cot.csv");
             double x = divisible * Math.PI / divider;
             double result = cot.calculate(x, eps);
             csvLogger.logger(x, result);
@@ -85,7 +103,7 @@ public class TrigFuncTest {
     @DisplayName("sec(x) test")
     void secTest(Double divisible, Double divider, Double trueResult) {
         try {
-            csvLogger.setFilePath("src/test/resources/results/sec.csv");
+            csvLogger.setFilePath("src/test/resources/results/trig/sec.csv");
             double x = divisible * Math.PI / divider;
             double result = sec.calculate(x, eps);
             csvLogger.logger(x, result);
@@ -100,7 +118,7 @@ public class TrigFuncTest {
     @DisplayName("csc(x) test")
     void cscTest(Double divisible, Double divider, Double trueResult) {
         try {
-            csvLogger.setFilePath("src/test/resources/results/csc.csv");
+            csvLogger.setFilePath("src/test/resources/results/trig/csc.csv");
             double x = divisible * Math.PI / divider;
             double result = csc.calculate(x, eps);
             csvLogger.logger(x, result);
@@ -115,7 +133,7 @@ public class TrigFuncTest {
     @DisplayName("trigonometric function test")
     void trigFuncTest(Double divisible, Double divider, Double trueResult) {
         try {
-            csvLogger.setFilePath("src/test/resources/results/trigFunc.csv");
+            csvLogger.setFilePath("src/test/resources/results/trig/trigFunc.csv");
             double x = divisible * Math.PI / divider;
             double result = trigonometricCalculator.calculate(x, eps);
             csvLogger.logger(x, result);
