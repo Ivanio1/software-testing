73f6c5cc2323399d7df069e979f4a162e0b69cce
Ivan Sobolev <isobolev@solanteq.com>
2024-04-12 23:00:58 +0300

    Working on profile page

--- tpo3/src/test/java/ru/ivanio/tpo/pages/HomePage.java
+++ tpo3/src/test/java/ru/ivanio/tpo/pages/HomePage.java
@@ -2,7 +2,6 @@ package ru.ivanio.tpo.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -18,14 +17,7 @@ public class HomePage extends Page {
     public WebElement signInButton;
     @FindBy(how = How.XPATH, using = "//*[@class=\"r-btn r-btn_large grey-lighter\"]")
     public WebElement registerButton;
-    @FindBy(how = How.XPATH, using = "//input[@value='']")
-    public WebElement findVacanciesInput;
 
-    @FindBy(how = How.XPATH, using = "//*[@class=\"vacancy-search-page__title-hint\"]/span")
-    public WebElement notFoundErrorText;
-//
-//    @FindBy(how = How.XPATH, using = "//li[2]/a")
-//    public WebElement enteredVacancyName;
     @FindBy(how = How.XPATH, using = "//*[@class=\"user-profile-menu__user-no-avatar\"]")
     public WebElement profileDiv;
     public HomePage(WebDriver webDriver) {
@@ -34,12 +26,6 @@ public class HomePage extends Page {
 
     public static HomePage initialize(WebDriver driver) {
         return Page.initialize(driver, "//*[@class=\"user-profile-header__sign-in r-btn r-btn_flat r-btn_large\"]", HomePage.class);
-    }
-
-    public void findVacancy(String vacancy) {
-        findVacanciesInput.click();
-        findVacanciesInput.sendKeys(vacancy);
-        findVacanciesInput.sendKeys(Keys.ENTER);
     }
 
     public void goToSignIn() {
@@ -52,6 +38,7 @@ public class HomePage extends Page {
         JavascriptExecutor executor = (JavascriptExecutor)driver;
         executor.executeScript("arguments[0].click();", element);
     }
+
 
     public void goToGeolocation() {
         WebElement element = driver.findElement(By.xpath("//*[@class=\"r-region-select__activator r-btn r-btn_flat r-btn_large\"]"));
