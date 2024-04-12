e9fad90bc06bbc4c339323e2401d419b989cd5e5
moyakkauntda <moyakkauntda@gmail.com>
2024-04-12 22:48:28 +0300

    lol

---
+++ tpo3/src/test/java/ru/ivanio/tpo/tests/HomePageTest.java
@@ -0,0 +1,47 @@
+package ru.ivanio.tpo.tests;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import ru.ivanio.tpo.pages.HomePage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HomePageTest extends PageTestBase {
+    HomePage homePage;
+    @Override
+    protected void preparePages(WebDriver driver) {
+        homePage = HomePage.initialize(driver);
+        homePage.acceptCookies();
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("allDrivers")
+    public void testNotFoundText(WebDriver driver) {
+        homePage.findVacancy("qweasdzxc");
+        new WebDriverWait(driver, 10).until(d -> homePage.notFoundErrorText.isDisplayed());
+        assertEquals(homePage.notFoundErrorText.getText(), "К сожалению, мы не нашли вакансий по вашему запросу. Что можно сделать?");
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("allDrivers")
+    public void testNotSelectedMetroText(WebDriver driver) {
+        homePage.radiusButton.click();
+        new WebDriverWait(driver, 10).until(d -> homePage.notSelectedTextHolder.isDisplayed() &&
+                homePage.selectMetroStationText.isDisplayed());
+        assertEquals(homePage.selectMetroStationText.getText(), "Выберите станции метро, рядом с которыми хотите найти работу");
+        assertEquals(homePage.notSelectedTextHolder.getText(), "Пока вы не выбрали ни одной станции");
+
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("allDrivers")
+    public void testFiltersCount(WebDriver driver) {
+        homePage.filterVacanciesForInvalids();
+        new WebDriverWait(driver, 10).until(d -> homePage.filtersCount.isDisplayed() );
+        assertEquals(homePage.filtersCount.getText(), "1");
+    }
+
+
+}
